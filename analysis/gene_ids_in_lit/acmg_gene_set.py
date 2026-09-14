40bb3c486817a3661fc63094d8fbbc1e1062e48b
Anastasia Bratulin <109570522+anastasiabratulin@users.noreply.github.com>
2026-09-14 10:44:41 -0400

    run all ACMG secondary genes through analysis

---
+++ analysis/gene_ids_in_lit/acmg_gene_set.py
@@ -0,0 +1,734 @@
+import functions as fn
+
+APC = fn.GenePair(
+    alias="APC",
+    approved_symbol="APC",
+    ncbi_gene_id="324",
+    hgnc_id="HGNC:583",
+    ensembl_gene_id="ENSG00000134982",
+    omim_id="611731"
+)
+MYH11 = fn.GenePair(
+    alias="MYH11",
+    approved_symbol="MYH11",
+    ncbi_gene_id="4629",
+    hgnc_id="HGNC:7569",
+    ensembl_gene_id="ENSG00000133392",
+    omim_id="160745"
+)
+ACTA2 = fn.GenePair(
+    alias="ACTA2",
+    approved_symbol="ACTA2",
+    ncbi_gene_id="59",
+    hgnc_id="HGNC:130",
+    ensembl_gene_id="ENSG00000107796",
+    omim_id="102620"
+)
+TMEM43 = fn.GenePair(
+    alias="TMEM43",
+    approved_symbol="TMEM43",
+    ncbi_gene_id="79188",
+    hgnc_id="HGNC:28472",
+    ensembl_gene_id="ENSG00000170876",
+    omim_id="612048"
+)
+DSP = fn.GenePair(
+    alias="DSP",
+    approved_symbol="DSP",
+    ncbi_gene_id="1832",
+    hgnc_id="HGNC:3052",
+    ensembl_gene_id="ENSG00000096696",
+    omim_id="125647"
+)
+PKP2 = fn.GenePair(
+    alias="PKP2",
+    approved_symbol="PKP2",
+    ncbi_gene_id="5318",
+    hgnc_id="HGNC:9024",
+    ensembl_gene_id="ENSG00000057294",
+    omim_id="602861"
+)
+DSG2 = fn.GenePair(
+    alias="DSG2",
+    approved_symbol="DSG2",
+    ncbi_gene_id="1829",
+    hgnc_id="HGNC:3049",
+    ensembl_gene_id="ENSG00000046604",
+    omim_id="125671"
+)
+DSC2 = fn.GenePair(
+    alias="DSC2",
+    approved_symbol="DSC2",
+    ncbi_gene_id="1824",
+    hgnc_id="HGNC:3036",
+    ensembl_gene_id="ENSG00000134755",
+    omim_id="125645"
+)
+BTD = fn.GenePair(
+    alias="BTD",
+    approved_symbol="BTD",
+    ncbi_gene_id="686",
+    hgnc_id="HGNC:1122",
+    ensembl_gene_id="ENSG00000169814",
+    omim_id="609019"
+)
+BRCA1 = fn.GenePair(
+    alias="BRCA1",
+    approved_symbol="BRCA1",
+    ncbi_gene_id="672",
+    hgnc_id="HGNC:1100",
+    ensembl_gene_id="ENSG00000012048",
+    omim_id="113705"
+)
+BRCA2 = fn.GenePair(
+    alias="BRCA2",
+    approved_symbol="BRCA2",
+    ncbi_gene_id="675",
+    hgnc_id="HGNC:1101",
+    ensembl_gene_id="ENSG00000139618",
+    omim_id="600185"
+)
+SCN5A = fn.GenePair(
+    alias="SCN5A",
+    approved_symbol="SCN5A",
+    ncbi_gene_id="6331",
+    hgnc_id="HGNC:10593",
+    ensembl_gene_id="ENSG00000183873",
+    omim_id="600163"
+)
+RYR2 = fn.GenePair(
+    alias="RYR2",
+    approved_symbol="RYR2",
+    ncbi_gene_id="6262",
+    hgnc_id="HGNC:10484",
+    ensembl_gene_id="ENSG00000198626",
+    omim_id="180902"
+)
+CASQ2 = fn.GenePair(
+    alias="CASQ2",
+    approved_symbol="CASQ2",
+    ncbi_gene_id="845",
+    hgnc_id="HGNC:1513",
+    ensembl_gene_id="ENSG00000118729",
+    omim_id="114251"
+)
+TRDN = fn.GenePair(
+    alias="TRDN",
+    approved_symbol="TRDN",
+    ncbi_gene_id="10345",
+    hgnc_id="HGNC:12261",
+    ensembl_gene_id="ENSG00000186439",
+    omim_id="603283"
+)
+LMNA = fn.GenePair(
+    alias="LMNA",
+    approved_symbol="LMNA",
+    ncbi_gene_id="4000",
+    hgnc_id="HGNC:6636",
+    ensembl_gene_id="ENSG00000160789",
+    omim_id="150330"
+)
+TNNC1 = fn.GenePair(
+    alias="TNNC1",
+    approved_symbol="TNNC1",
+    ncbi_gene_id="7134",
+    hgnc_id="HGNC:11943",
+    ensembl_gene_id="ENSG00000114854",
+    omim_id="191040"
+)
+RBM20 = fn.GenePair(
+    alias="RBM20",
+    approved_symbol="RBM20",
+    ncbi_gene_id="282996",
+    hgnc_id="HGNC:27424",
+    ensembl_gene_id="ENSG00000203867",
+    omim_id="613171"
+)
+TTN = fn.GenePair(
+    alias="TTN",
+    approved_symbol="TTN",
+    ncbi_gene_id="7273",
+    hgnc_id="HGNC:12403",
+    ensembl_gene_id="ENSG00000155657",
+    omim_id="188840"
+)
+COL3A1 = fn.GenePair(
+    alias="COL3A1",
+    approved_symbol="COL3A1",
+    ncbi_gene_id="1281",
+    hgnc_id="HGNC:2201",
+    ensembl_gene_id="ENSG00000168542",
+    omim_id="120180"
+)
+GLA = fn.GenePair(
+    alias="GLA",
+    approved_symbol="GLA",
+    ncbi_gene_id="1281",
+    hgnc_id="HGNC:2201",
+    ensembl_gene_id="ENSG00000168542",
+    omim_id="300644"
+)
+LDLR = fn.GenePair(
+    alias="LDLR",
+    approved_symbol="LDLR",
+    ncbi_gene_id="3949",
+    hgnc_id="HGNC:6547",
+    ensembl_gene_id="ENSG00000130164",
+    omim_id="606945"
+)
+APOB = fn.GenePair(
+    alias="APOB",
+    approved_symbol="APOB",
+    ncbi_gene_id="338",
+    hgnc_id="HGNC:603",
+    ensembl_gene_id="ENSG00000084674",
+    omim_id="107730"
+)
+MYH7 = fn.GenePair(
+    alias="MYH7",
+    approved_symbol="MYH7",
+    ncbi_gene_id="4625",
+    hgnc_id="HGNC:7577",
+    ensembl_gene_id="ENSG00000092054",
+    omim_id="160760"
+)
+TNNT2 = fn.GenePair(
+    alias="TNNT2",
+    approved_symbol="TNNT2",
+    ncbi_gene_id="7139",
+    hgnc_id="HGNC:11949",
+    ensembl_gene_id="ENSG00000118194",
+    omim_id="191045"
+)
+TPM1 = fn.GenePair(
+    alias="TPM1",
+    approved_symbol="TPM1",
+    ncbi_gene_id="7168",
+    hgnc_id="HGNC:12010",
+    ensembl_gene_id="ENSG00000140416",
+    omim_id="191010"
+)
+MYBPC3 = fn.GenePair(
+    alias="MYBPC3",
+    approved_symbol="MYBPC3",
+    ncbi_gene_id="4607",
+    hgnc_id="HGNC:7551",
+    ensembl_gene_id="ENSG00000134571",
+    omim_id="600958"
+)
+PRKAG2 = fn.GenePair(
+    alias="PRKAG2",
+    approved_symbol="PRKAG2",
+    ncbi_gene_id="51422",
+    hgnc_id="HGNC:9386",
+    ensembl_gene_id="ENSG00000106617",
+    omim_id="602743"
+)
+TNNI3 = fn.GenePair(
+    alias="TNNI3",
+    approved_symbol="TNNI3",
+    ncbi_gene_id="7137",
+    hgnc_id="HGNC:11947",
+    ensembl_gene_id="ENSG00000129991",
+    omim_id="191044"
+)
+MYL3 = fn.GenePair(
+    alias="MYL3",
+    approved_symbol="MYL3",
+    ncbi_gene_id="4634",
+    hgnc_id="HGNC:7584",
+    ensembl_gene_id="ENSG00000160808",
+    omim_id="160790"
+)
+MYL2 = fn.GenePair(
+    alias="MYL2",
+    approved_symbol="MYL2",
+    ncbi_gene_id="4633",
+    hgnc_id="HGNC:7583",
+    ensembl_gene_id="ENSG00000111245",
+    omim_id="160781"
+)
+ACTC1 = fn.GenePair(
+    alias="ACTC1",
+    approved_symbol="ACTC1",
+    ncbi_gene_id="70",
+    hgnc_id="HGNC:143",
+    ensembl_gene_id="ENSG00000159251",
+    omim_id="102540"
+)
+PALB2 = fn.GenePair(
+    alias="PALB2",
+    approved_symbol="PALB2",
+    ncbi_gene_id="79728",
+    hgnc_id="HGNC:26144",
+    ensembl_gene_id="ENSG00000083093",
+    omim_id="610355"
+)
+HFE = fn.GenePair(
+    alias="HFE",
+    approved_symbol="HFE",
+    ncbi_gene_id="3077",
+    hgnc_id="HGNC:4886",
+    ensembl_gene_id="ENSG00000010704",
+    omim_id="613609"
+)
+ENG = fn.GenePair(
+    alias="ENG",
+    approved_symbol="ENG",
+    ncbi_gene_id="2022",
+    hgnc_id="HGNC:3349",
+    ensembl_gene_id="ENSG00000106991",
+    omim_id="131195"
+)
+ACVRL1 = fn.GenePair(
+    alias="ACVRL1",
+    approved_symbol="ACVRL1",
+    ncbi_gene_id="94",
+    hgnc_id="HGNC:175",
+    ensembl_gene_id="ENSG00000139567",
+    omim_id="601284"
+)
+SDHD = fn.GenePair(
+    alias="SDHD",
+    approved_symbol="SDHD",
+    ncbi_gene_id="6392",
+    hgnc_id="HGNC:10683",
+    ensembl_gene_id="ENSG00000204370",
+    omim_id="602690"
+)
+SDHB = fn.GenePair(
+    alias="SDHB",
+    approved_symbol="SDHB",
+    ncbi_gene_id="6390",
+    hgnc_id="HGNC:10681",
+    ensembl_gene_id="ENSG00000117118",
+    omim_id="185470"
+)
+TTR = fn.GenePair(
+    alias="TTR",
+    approved_symbol="TTR",
+    ncbi_gene_id="7276",
+    hgnc_id="HGNC:12405",
+    ensembl_gene_id="ENSG00000118271",
+    omim_id="176300"
+)
+PCSK9 = fn.GenePair(
+    alias="PCSK9",
+    approved_symbol="PCSK9",
+    ncbi_gene_id="255738",
+    hgnc_id="HGNC:20001",
+    ensembl_gene_id="ENSG00000169174",
+    omim_id="607786"
+)
+BMPR1A = fn.GenePair(
+    alias="BMPR1A",
+    approved_symbol="BMPR1A",
+    ncbi_gene_id="657",
+    hgnc_id="HGNC:1076",
+    ensembl_gene_id="ENSG00000107779",
+    omim_id="601299"
+)
+SMAD4 = fn.GenePair(
+    alias="SMAD4",
+    approved_symbol="SMAD4",
+    ncbi_gene_id="4089",
+    hgnc_id="HGNC:6770",
+    ensembl_gene_id="ENSG00000141646",
+    omim_id="600993"
+)
+TP53 = fn.GenePair(
+    alias="TP53",
+    approved_symbol="TP53",
+    ncbi_gene_id="7157",
+    hgnc_id="HGNC:11998",
+    ensembl_gene_id="ENSG00000141510",
+    omim_id="191170"
+)
+TGFBR1 = fn.GenePair(
+    alias="TGFBR1",
+    approved_symbol="TGFBR1",
+    ncbi_gene_id="7046",
+    hgnc_id="HGNC:11772",
+    ensembl_gene_id="ENSG00000106799",
+    omim_id="190181"
+)
+TGFBR2 = fn.GenePair(
+    alias="TGFBR2",
+    approved_symbol="TGFBR2",
+    ncbi_gene_id="7048",
+    hgnc_id="HGNC:11773",
+    ensembl_gene_id="ENSG00000163513",
+    omim_id="190182"
+)
+SMAD3 = fn.GenePair(
+    alias="SMAD3",
+    approved_symbol="SMAD3",
+    ncbi_gene_id="4088",
+    hgnc_id="HGNC:6769",
+    ensembl_gene_id="ENSG00000166949",
+    omim_id="603109"
+)
+KCNQ1 = fn.GenePair(
+    alias="KCNQ1",
+    approved_symbol="KCNQ1",
+    ncbi_gene_id="3784",
+    hgnc_id="HGNC:6294",
+    ensembl_gene_id="ENSG00000053918",
+    omim_id="607542"
+)
+KCNH2 = fn.GenePair(
+    alias="KCNH2",
+    approved_symbol="KCNH2",
+    ncbi_gene_id="3757",
+    hgnc_id="HGNC:6251",
+    ensembl_gene_id="ENSG00000055118",
+    omim_id="152427"
+)
+CALM1 = fn.GenePair(
+    alias="CALM1",
+    approved_symbol="CALM1",
+    ncbi_gene_id="801",
+    hgnc_id="HGNC:1442",
+    ensembl_gene_id="ENSG00000198668",
+    omim_id="114180"
+)
+CALM2 = fn.GenePair(
+    alias="CALM2",
+    approved_symbol="CALM2",
+    ncbi_gene_id="805",
+    hgnc_id="HGNC:1445",
+    ensembl_gene_id="ENSG00000143933",
+    omim_id="114182"
+)
+CALM3 = fn.GenePair(
+    alias="CALM3",
+    approved_symbol="CALM3",
+    ncbi_gene_id="808",
+    hgnc_id="HGNC:1449",
+    ensembl_gene_id="ENSG00000160014",
+    omim_id="114183"
+)
+MSH2 = fn.GenePair(
+    alias="MSH2",
+    approved_symbol="MSH2",
+    ncbi_gene_id="4436",
+    hgnc_id="HGNC:7325",
+    ensembl_gene_id="ENSG00000095002",
+    omim_id="609309"
+)
+MLH1 = fn.GenePair(
+    alias="MLH1",
+    approved_symbol="MLH1",
+    ncbi_gene_id="4292",
+    hgnc_id="HGNC:7127",
+    ensembl_gene_id="ENSG00000076242",
+    omim_id="120436"
+)
+PMS2 = fn.GenePair(
+    alias="PMS2",
+    approved_symbol="PMS2",
+    ncbi_gene_id="5395",
+    hgnc_id="HGNC:9122",
+    ensembl_gene_id="ENSG00000122512",
+    omim_id="600259"
+)
+MSH6 = fn.GenePair(
+    alias="MSH6",
+    approved_symbol="MSH6",
+    ncbi_gene_id="2956",
+    hgnc_id="HGNC:7329",
+    ensembl_gene_id="ENSG00000116062",
+    omim_id="600678"
+)
+RYR1 = fn.GenePair(
+    alias="RYR1",
+    approved_symbol="RYR1",
+    ncbi_gene_id="6261",
+    hgnc_id="HGNC:10483",
+    ensembl_gene_id="ENSG00000196218",
+    omim_id="180901"
+)
+CACNA1S = fn.GenePair(
+    alias="CACNA1S",
+    approved_symbol="CACNA1S",
+    ncbi_gene_id="779",
+    hgnc_id="HGNC:1397",
+    ensembl_gene_id="ENSG00000081248",
+    omim_id="114208"
+)
+FBN1 = fn.GenePair(
+    alias="FBN1",
+    approved_symbol="FBN1",
+    ncbi_gene_id="2200",
+    hgnc_id="HGNC:3603",
+    ensembl_gene_id="ENSG00000166147",
+    omim_id="134797"
+)
+HNF1A = fn.GenePair(
+    alias="HNF1A",
+    approved_symbol="HNF1A",
+    ncbi_gene_id="6927",
+    hgnc_id="HGNC:11621",
+    ensembl_gene_id="ENSG00000135100",
+    omim_id="142410"
+)
+MEN1 = fn.GenePair(
+    alias="MEN1",
+    approved_symbol="MEN1",
+    ncbi_gene_id="4221",
+    hgnc_id="HGNC:7010",
+    ensembl_gene_id="ENSG00000133895",
+    omim_id="613733"
+)
+RET = fn.GenePair(
+    alias="RET",
+    approved_symbol="RET",
+    ncbi_gene_id="5979",
+    hgnc_id="HGNC:9967",
+    ensembl_gene_id="ENSG00000165731",
+    omim_id="164761"
+)
+MUTYH = fn.GenePair(
+    alias="MUTYH",
+    approved_symbol="MUTYH",
+    ncbi_gene_id="4595",
+    hgnc_id="HGNC:7527",
+    ensembl_gene_id="ENSG00000132781",
+    omim_id="604933"
+)
+DES = fn.GenePair(
+    alias="DES",
+    approved_symbol="DES",
+    ncbi_gene_id="1674",
+    hgnc_id="HGNC:2770",
+    ensembl_gene_id="ENSG00000175084",
+    omim_id="125660"
+)
+FLNC = fn.GenePair(
+    alias="FLNC",
+    approved_symbol="FLNC",
+    ncbi_gene_id="2318",
+    hgnc_id="HGNC:3756",
+    ensembl_gene_id="ENSG00000128591",
+    omim_id="102565"
+)
+BAG3 = fn.GenePair(
+    alias="BAG3",
+    approved_symbol="BAG3",
+    ncbi_gene_id="9531",
+    hgnc_id="HGNC:939",
+    ensembl_gene_id="ENSG00000151929",
+    omim_id="603883"
+)
+NF2 = fn.GenePair(
+    alias="NF2",
+    approved_symbol="NF2",
+    ncbi_gene_id="4771",
+    hgnc_id="HGNC:7773",
+    ensembl_gene_id="ENSG00000186575",
+    omim_id="607379"
+)
+OTC = fn.GenePair(
+    alias="OTC",
+    approved_symbol="OTC",
+    ncbi_gene_id="5009",
+    hgnc_id="HGNC:8512",
+    ensembl_gene_id="ENSG00000036473",
+    omim_id="300461"
+)
+SDHAF2 = fn.GenePair(
+    alias="SDHAF2",
+    approved_symbol="SDHAF2",
+    ncbi_gene_id="54949",
+    hgnc_id="HGNC:26034",
+    ensembl_gene_id="ENSG00000167985",
+    omim_id="613019"
+)
+SDHC = fn.GenePair(
+    alias="SDHC",
+    approved_symbol="SDHC",
+    ncbi_gene_id="6391",
+    hgnc_id="HGNC:10682",
+    ensembl_gene_id="ENSG00000143252",
+    omim_id="602413"
+)
+STK11 = fn.GenePair(
+    alias="STK11",
+    approved_symbol="STK11",
+    ncbi_gene_id="6794",
+    hgnc_id="HGNC:11389",
+    ensembl_gene_id="ENSG00000118046",
+    omim_id="602216"
+)
+MAX = fn.GenePair(
+    alias="MAX",
+    approved_symbol="MAX",
+    ncbi_gene_id="4149",
+    hgnc_id="HGNC:6913",
+    ensembl_gene_id="ENSG00000125952",
+    omim_id="154950"
+)
+TMEM127 = fn.GenePair(
+    alias="TMEM127",
+    approved_symbol="TMEM127",
+    ncbi_gene_id="55654",
+    hgnc_id="HGNC:26038",
+    ensembl_gene_id="ENSG00000135956",
+    omim_id="613403"
+)
+GAA = fn.GenePair(
+    alias="GAA",
+    approved_symbol="GAA",
+    ncbi_gene_id="2548",
+    hgnc_id="HGNC:4065",
+    ensembl_gene_id="ENSG00000171298",
+    omim_id="606800"
+)
+PTEN = fn.GenePair(
+    alias="PTEN",
+    approved_symbol="PTEN",
+    ncbi_gene_id="5728",
+    hgnc_id="HGNC:9588",
+    ensembl_gene_id="ENSG00000171862",
+    omim_id="601728"
+)
+RB1 = fn.GenePair(
+    alias="RB1",
+    approved_symbol="RB1",
+    ncbi_gene_id="5925",
+    hgnc_id="HGNC:9884",
+    ensembl_gene_id="ENSG00000139687",
+    omim_id="614041"
+)
+RPE65 = fn.GenePair(
+    alias="RPE65",
+    approved_symbol="RPE65",
+    ncbi_gene_id="6121",
+    hgnc_id="HGNC:10294",
+    ensembl_gene_id="ENSG00000116745",
+    omim_id="180069"
+)
+TSC1 = fn.GenePair(
+    alias="TSC1",
+    approved_symbol="TSC1",
+    ncbi_gene_id="7248",
+    hgnc_id="HGNC:12362",
+    ensembl_gene_id="ENSG00000165699",
+    omim_id="605284"
+)
+TSC2 = fn.GenePair(
+    alias="TSC2",
+    approved_symbol="TSC2",
+    ncbi_gene_id="7249",
+    hgnc_id="HGNC:12363",
+    ensembl_gene_id="ENSG00000103197",
+    omim_id="191092"
+)
+VHL = fn.GenePair(
+    alias="VHL",
+    approved_symbol="VHL",
+    ncbi_gene_id="7428",
+    hgnc_id="HGNC:12687",
+    ensembl_gene_id="ENSG00000134086",
+    omim_id="608537"
+)
+WT1 = fn.GenePair(
+    alias="WT1",
+    approved_symbol="WT1",
+    ncbi_gene_id="7490",
+    hgnc_id="HGNC:12796",
+    ensembl_gene_id="ENSG00000184937",
+    omim_id="607102"
+)
+ATP7B = fn.GenePair(
+    alias="ATP7B",
+    approved_symbol="ATP7B",
+    ncbi_gene_id="540",
+    hgnc_id="HGNC:870",
+    ensembl_gene_id="ENSG00000123191",
+    omim_id="606882"
+)
+
+GENES = [
+    APC,
+    MYH11,
+    ACTA2,
+    TMEM43,
+    DSP,
+    PKP2,
+    DSG2,
+    DSC2,
+    BTD,
+    BRCA1,
+    BRCA2,
+    SCN5A,
+    RYR2,
+    CASQ2,
+    TRDN,
+    LMNA,
+    TNNC1,
+    RBM20,
+    TTN,
+    COL3A1,
+    GLA,
+    LDLR,
+    APOB,
+    MYH7,# stopped here uploading to S3
+    TNNT2,
+    TPM1,
+    MYBPC3,
+    PRKAG2,
+    TNNI3,
+    MYL3,
+    MYL2,
+    ACTC1,
+    PALB2,
+    HFE,
+    ENG,
+    ACVRL1,
+    SDHD,
+    SDHB,
+    TTR,
+    PCSK9,
+    BMPR1A,
+    SMAD4,
+    TP53,
+    TGFBR1,
+    TGFBR2,
+    SMAD3,
+    KCNQ1,
+    KCNH2,
+    CALM1,
+    CALM2,
+    CALM3,
+    MSH2,
+    MLH1,
+    PMS2,
+    MSH6,
+    RYR1,
+    CACNA1S,
+    FBN1,
+    HNF1A,
+    MEN1,
+    RET,
+    MUTYH,
+    DES,
+    FLNC,
+    BAG3,
+    NF2,
+    OTC,
+    SDHAF2,
+    SDHC,
+    STK11,
+    MAX,
+    TMEM127,
+    GAA,
+    PTEN,
+    RB1,
+    RPE65,
+    TSC1,
+    TSC2,
+    VHL,
+    WT1,
+    ATP7B,
+]
